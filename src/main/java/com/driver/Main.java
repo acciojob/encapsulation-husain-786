8ce69a47b4db98b08124b5f7258e2b04a4c6c48e
husain-786 <63996307+husain-786@users.noreply.github.com>
2023-05-14 14:01:17 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,10 @@ public class Main
 	   // o/p:- name has private access in RWOnly
 	   // when try to access the above data member we get this error because
 	   // here the data memeber is private.....
-	   
+
+	   // System.out.println(r.name);
+	   // will not be able to print as name field is private and so will not be acceesible....
+		
 	   // we can access the private data member from another class by using 
 	   // getter and setter.....
 	   r.setName("Accio");
